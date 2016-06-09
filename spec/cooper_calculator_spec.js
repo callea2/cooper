631def6c79176660e015fec965913420ef7b0dde
Carl-Philip <callea2@gmail.com>
2016-06-09 13:09:57 +0200

    Added additional test for VO2 and made it pass To get a more accurate results you have to factor in age when calculating VO2 max, therfore I added a test for my application to factor that in and then implemented the code for it to pass

--- spec/cooper_calculator_spec.js
+++ spec/cooper_calculator_spec.js
@@ -1,23 +1,29 @@
 describe("V02 Max", function(){
 
   beforeEach(function() {
-    person = new Person({distance: 2200, gender: 'female'});
+    person = new Person({distance: 2220, gender: 'male', age: 21});
     calculator = new CPRCalculator();
     calculator.VO2_max(person);
   });
 
   it("should calculate VO2 max", function() {
-    expect(person.VO2Value).toEqual(37.9);
+    expect(person.VO2Value).toEqual(38.34);
   });
 
   it("should display custom VO2 message based on gender (female)", function() {
-    expect(person.VO2Message).toEqual("Below average")
+    person = new Person({distance: 2220, gender: 'female', age: 21});
+    calculator.VO2_max(person);
+    expect(person.VO2Message).toEqual("Average")
   });
 
   it("should display custom VO2 message based on gender (male)", function() {
-    person = new Person({distance: 2200, gender: 'male'});
+    expect(person.VO2Message).toEqual("Below average")
+  });
+
+  it("should display custom VO2 message based on age", function() {
+    person = new Person({distance: 2200, gender: 'male', age: 65});
     calculator.VO2_max(person);
-    expect(person.VO2Message).toEqual("Poor")
+    expect(person.VO2Message).toEqual("Very good")
   });
 });
 
--- src/cooper_calculator.js
+++ src/cooper_calculator.js
@@ -11,10 +11,296 @@ CPRCalculator.prototype.VO2_max = function(obj) {
 };
 
 function setVO2Message (obj, value){
-  if (obj.gender == 'male' && obj.VO2Value < 40) {
-    obj.VO2Message = "Poor"
+  if (obj.gender == 'male' && obj.age >= 18 && obj.age <= 25) {
+    if (obj.VO2Value < 30) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 30 && obj.VO2Value <= 36) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 37 && obj.VO2Value <= 41) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 42 && obj.VO2Value <= 46) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 47 && obj.VO2Value <= 51) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 52 && obj.VO2Value <= 60) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 60) {
+      obj.VO2Message = "Excellent"
+    }
   }
-  if (obj.gender == 'female' && obj.VO2Value < 40) {
-    obj.VO2Message = "Below average"
+
+  else if (obj.gender == 'male' && obj.age >= 26 && obj.age <= 35) {
+    if (obj.VO2Value < 30) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 30 && obj.VO2Value <= 34) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 35 && obj.VO2Value <= 39) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 40 && obj.VO2Value <= 42) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 43 && obj.VO2Value <= 48) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 49 && obj.VO2Value <= 56) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 56) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'male' && obj.age >= 36 && obj.age <= 45) {
+    if (obj.VO2Value < 26) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 26 && obj.VO2Value <= 30) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 31 && obj.VO2Value <= 34) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 35 && obj.VO2Value <= 38) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 39 && obj.VO2Value <= 42) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 43 && obj.VO2Value <= 51) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 51) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'male' && obj.age >= 46 && obj.age <= 55) {
+    if (obj.VO2Value < 25) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 25 && obj.VO2Value <= 28) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 29 && obj.VO2Value <= 31) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 32 && obj.VO2Value <= 35) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 36 && obj.VO2Value <= 38) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 39 && obj.VO2Value <= 45) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 45) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'male' && obj.age >= 56 && obj.age <= 65) {
+    if (obj.VO2Value < 22) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 22 && obj.VO2Value <= 25) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 26 && obj.VO2Value <= 29) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 30 && obj.VO2Value <= 31) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 32 && obj.VO2Value <= 35) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 36 && obj.VO2Value <= 41) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 41) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'male' && obj.age > 65) {
+    if (obj.VO2Value < 20) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 20 && obj.VO2Value <= 21) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 22 && obj.VO2Value <= 25) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 26 && obj.VO2Value <= 28) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 29 && obj.VO2Value <= 32) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 33 && obj.VO2Value <= 37) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 37) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+
+
+
+
+
+  else if (obj.gender == 'female' && obj.age >= 18 && obj.age <= 25) {
+    if (obj.VO2Value < 28) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 28 && obj.VO2Value <= 32) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 33 && obj.VO2Value <= 37) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 38 && obj.VO2Value <= 41) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 42 && obj.VO2Value <= 46) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 47 && obj.VO2Value <= 56) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 56) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'female' && obj.age >= 26 && obj.age <= 35) {
+    if (obj.VO2Value < 26) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 26 && obj.VO2Value <= 30) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 31 && obj.VO2Value <= 34) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 35 && obj.VO2Value <= 38) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 39 && obj.VO2Value <= 44) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 45 && obj.VO2Value <= 52) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 52) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'female' && obj.age >= 36 && obj.age <= 45) {
+    if (obj.VO2Value < 22) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 22 && obj.VO2Value <= 26) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 27 && obj.VO2Value <= 30) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 31 && obj.VO2Value <= 33) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 34 && obj.VO2Value <= 37) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 38 && obj.VO2Value <= 45) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 45) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'female' && obj.age >= 46 && obj.age <= 55) {
+    if (obj.VO2Value < 20) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 20 && obj.VO2Value <= 24) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 25 && obj.VO2Value <= 27) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 28 && obj.VO2Value <= 30) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 31 && obj.VO2Value <= 33) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 34 && obj.VO2Value <= 40) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 40) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'female' && obj.age >= 56 && obj.age <= 65) {
+    if (obj.VO2Value < 18) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 18 && obj.VO2Value <= 21) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 22 && obj.VO2Value <= 24) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 25 && obj.VO2Value <= 27) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 28 && obj.VO2Value <= 31) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 32 && obj.VO2Value <= 37) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 37) {
+      obj.VO2Message = "Excellent"
+    }
+  }
+
+  else if (obj.gender == 'female' && obj.age > 65) {
+    if (obj.VO2Value < 17) {
+      obj.VO2Message = "Very poor"
+    }
+    else if (obj.VO2Value >= 17 && obj.VO2Value <= 18) {
+      obj.VO2Message = "Poor"
+    }
+    else if (obj.VO2Value >= 19 && obj.VO2Value <= 21) {
+      obj.VO2Message = "Below average"
+    }
+    else if (obj.VO2Value >= 22 && obj.VO2Value <= 24) {
+      obj.VO2Message = "Average"
+    }
+    else if (obj.VO2Value >= 25 && obj.VO2Value <= 27) {
+      obj.VO2Message = "Above Average"
+    }
+    else if (obj.VO2Value >= 28 && obj.VO2Value <= 32) {
+      obj.VO2Message = "Very good"
+    }
+    else if (obj.VO2Value > 32) {
+      obj.VO2Message = "Excellent"
+    }
   }
 };
