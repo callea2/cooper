8520ffc44b3aac4299265ed5b61bd4b1da4368f1
Carl-Philip <callea2@gmail.com>
2016-06-15 13:00:07 +0200

    Updated ApplicationController to fix CRSF token issue

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::PerformanceDataController < ApplicationController
-  before_action :authenticate_api_v1_user!
 
   def create
     @data = PerformanceData.new(performance_data_params.merge!({user: current_api_v1_user}))
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token
   include DeviseTokenAuth::Concerns::SetUserByToken
   respond_to :json
 end
