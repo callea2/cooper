cfdc9930a86d7d1fba5663dbdc495ccd8addd266
Carl-Philip <callea2@gmail.com>
2016-06-15 12:37:54 +0200

    Updated ApplicationController to fix CRSF token issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_filter :verify_authenticity_token
   include DeviseTokenAuth::Concerns::SetUserByToken
   respond_to :json
 end
